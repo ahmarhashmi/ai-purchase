499ea62a66ea2aec508d025ec5c4bb29b9e28b42
Abdul Rehma Azam <arehman@Abduls-MacBook-Pro-2.local>
2023-03-14 19:24:13 +0500

    Added all codes, data, model and docker API

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ai-purchase
 An intelligent solution to identify the items that user wants to purchase. It extracts basic fields for item creation and returns a JSON.
+
+# Deployment 
+Steps to deploy the model present in the ner_model 
+
+1) Change the directory to ``ner_model`` directory.
+2) Build the docker with follwing command.
+```angular2html
+docker build . -t name:version 
+```
+3) Run the docker image with the following command
+```angular2html
+docker run -d --name name_of_container -p port:port name_of_image:version_of_image
+```
+4) Request the POST API using following url :
+```angular2html
+http://[url]:[port]/predict/
+```
---
+++ ner_model/app/main_function.py
@@ -0,0 +1,74 @@
+import logging
+import spacy
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+model = spacy.load('app/model/en_pr_vroozi_new')
+
+def model_predictions(input):
+    items = {}
+    try:
+       global model
+       doc = model(input['text'])
+       product = {}
+       suppliers = 'None'
+       count = 0
+       for ent in doc.ents:
+           if ent.label_ == "Quantity":
+               product["Quantity"] = ent.text
+           elif ent.label_ == "Product":
+               product["Product"] = ent.text
+           elif ent.label_ == "Price":
+               product["Price"] = ent.text
+           elif ent.label_ == "Supplier":
+               suppliers = ent.text
+           options = list(product.keys())
+           if "Quantity" in options and "Product" in options and "Price" in options:
+               product['Supplier'] = suppliers
+               items['item_' + str(count)] = product
+               product = {}
+               count += 1
+       for i in range(count):
+           items['item_' + str(i)]['Supplier'] = suppliers
+    except Exception as e:
+        logging.Logger.error(self=logging.Logger(name='model_predictions'),msg=str(e))
+    finally:
+        return items
+
+class request_body(BaseModel):
+    text : str
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Intro": "Model for prediction of Donation amounts of  "}
+
+
+@app.post('/predict')
+def prediction_function(data : request_body):
+    predictions='None'
+    try:
+        input_payload={
+            'text' :data.text,
+        }
+        predictions=model_predictions(input_payload)
+        logging.Logger.info(predictions)
+
+    except Exception as e:
+        logging.Logger.error(self=logging.Logger(name='Prediction_Function'),msg='Prediction: '+ str(predictions))
+    finally:
+        return predictions
+
+
+if __name__ == '__main__':
+    payload={ 'text':'I want to buy 2 boxes of pasta for $4 and 1 pack of orange juice costing $3 from IJAZ EMAIL'
+    }
+    model_predictions(payload)
+
+
+
+
+
